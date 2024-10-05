3c41669f4df1fc9d68a2fa9f13a9238c7405e149
Ian Johnson <ian@ianjohnson.dev>
2024-10-04 23:36:34 -0400

    feat: implement setting default

--- src/zigman.zig
+++ src/zigman.zig
@@ -72,6 +72,7 @@ const zigman_usage =
     \\
     \\Commands:
     \\
+    \\  default VERSION                 Set Zig VERSION as the default
     \\  graft PATH                      Grafts/copies an existing Zig at PATH into Zigman
     \\  install [VERSION]               Install Zig VERSION (default 'latest')
     \\  list                            List installed Zig versions
@@ -88,7 +89,9 @@ const zigman_usage =
 fn exeZigman(gpa: Allocator, args: []const []const u8) !void {
     if (args.len == 0) return fatal("no command specified", .{});
 
-    if (std.mem.eql(u8, args[0], "graft")) {
+    if (std.mem.eql(u8, args[0], "default")) {
+        try cmdDefault(gpa, args[1..]);
+    } else if (std.mem.eql(u8, args[0], "graft")) {
         try cmdGraft(gpa, args[1..]);
     } else if (std.mem.eql(u8, args[0], "install")) {
         try cmdInstall(gpa, args[1..]);
@@ -109,6 +112,22 @@ fn exeZigman(gpa: Allocator, args: []const []const u8) !void {
     }
 }
 
+fn cmdDefault(gpa: Allocator, args: []const []const u8) !void {
+    if (args.len == 0) return fatal("no version provided", .{});
+    if (args.len > 1) return fatal("too many arguments", .{});
+
+    var data = try AppData.open(gpa);
+    defer data.close();
+    const available = try data.loadAvailableInstallations(gpa);
+    defer gpa.free(available);
+    const new_default = ZigInstallation.Query.parse(args[0]) catch
+        return fatal("invalid Zig version", .{});
+    if (findMatchingInstallationIndex(available, new_default) == null) {
+        return fatal("no matching Zig version installed", .{});
+    }
+    try data.saveDefault(new_default);
+}
+
 fn cmdGraft(gpa: Allocator, args: []const []const u8) !void {
     if (args.len == 0) return fatal("no path provided", .{});
     if (args.len > 1) return fatal("too many arguments", .{});
@@ -126,14 +145,13 @@ fn cmdInstall(gpa: Allocator, args: []const []const u8) !void {
 fn cmdList(gpa: Allocator, args: []const []const u8) !void {
     if (args.len != 0) return fatal("too many arguments", .{});
 
-    const available = loadAvailableInstallations(gpa) catch |err|
-        return fatal("failed to load Zig installations: {}", .{err});
+    var data = try AppData.open(gpa);
+    defer data.close();
+    const available = try data.loadAvailableInstallations(gpa);
     defer gpa.free(available);
     std.sort.pdq(ZigInstallation, available, {}, struct {
         fn lessThan(ctx: void, lhs: ZigInstallation, rhs: ZigInstallation) bool {
             _ = ctx;
-            if (lhs.default) return true;
-            if (rhs.default) return false;
             const lhs_version = lhs.semanticVersion();
             const lhs_is_mach = std.mem.startsWith(u8, lhs_version.pre orelse "", "mach");
             const rhs_version = rhs.semanticVersion();
@@ -143,13 +161,35 @@ fn cmdList(gpa: Allocator, args: []const []const u8) !void {
             return lhs_version.order(rhs_version) == .gt;
         }
     }.lessThan);
+    const master_index = findMatchingInstallationIndex(available, .master);
+    const latest_index = findMatchingInstallationIndex(available, .latest);
+    const mach_latest_index = findMatchingInstallationIndex(available, .mach_latest);
+    const default_index = if (try data.loadDefault()) |default|
+        findMatchingInstallationIndex(available, default)
+    else
+        null;
 
     const tty_config = std.io.tty.detectConfig(std.io.getStdOut());
     var stdout_buf = std.io.bufferedWriter(std.io.getStdOut().writer());
     const stdout = stdout_buf.writer();
-    for (available) |zig| {
+    for (available, 0..) |zig, i| {
         try stdout.print("{s}", .{zig.version.slice()});
-        if (zig.default) {
+        if (i == master_index) {
+            try tty_config.setColor(stdout, .bright_white);
+            try stdout.writeAll(" master");
+            try tty_config.setColor(stdout, .reset);
+        }
+        if (i == latest_index) {
+            try tty_config.setColor(stdout, .bright_white);
+            try stdout.writeAll(" latest");
+            try tty_config.setColor(stdout, .reset);
+        }
+        if (i == mach_latest_index) {
+            try tty_config.setColor(stdout, .bright_white);
+            try stdout.writeAll(" mach-latest");
+            try tty_config.setColor(stdout, .reset);
+        }
+        if (i == default_index) {
             try tty_config.setColor(stdout, .bright_white);
             try stdout.writeAll(" default");
             try tty_config.setColor(stdout, .reset);
@@ -170,35 +210,48 @@ fn cmdRemove(gpa: Allocator, args: []const []const u8) !void {
     if (args.len == 0) return fatal("no version provided", .{});
     if (args.len > 1) return fatal("too many arguments", .{});
 
-    const available = loadAvailableInstallations(gpa) catch |err|
-        return fatal("failed to load Zig installations: {}", .{err});
+    var data = try AppData.open(gpa);
+    defer data.close();
+    const available = try data.loadAvailableInstallations(gpa);
     defer gpa.free(available);
-    const remove_index = findMatchingInstallationIndex(available, .parse(args[0])) orelse
+    const query = ZigInstallation.Query.parse(args[0]) catch
+        return fatal("invalid Zig version", .{});
+    const remove_index = findMatchingInstallationIndex(available, query) orelse
         return fatal("no Zig matching '{s}'", .{args[0]});
     const new_available = try gpa.alloc(ZigInstallation, available.len - 1);
     defer gpa.free(new_available);
     @memcpy(new_available[0..remove_index], available[0..remove_index]);
     @memcpy(new_available[remove_index..], available[remove_index + 1 ..]);
-    saveAvailableInstallations(gpa, new_available) catch |err|
-        return fatal("failed to save Zig installations: {}", .{err});
+    try data.saveAvailableInstallations(new_available);
 }
 
 fn cmdRun(gpa: Allocator, args: []const []const u8) !noreturn {
     if (args.len == 0) return fatal("no version provided", .{});
 
-    const available = loadAvailableInstallations(gpa) catch |err|
-        return fatal("failed to load Zig installations: {}", .{err});
+    var data = try AppData.open(gpa);
+    defer data.close();
+    const available = try data.loadAvailableInstallations(gpa);
     defer gpa.free(available);
-    const zig_index = findMatchingInstallationIndex(available, .parse(args[0])) orelse
+    const query = query: {
+        if (std.mem.eql(u8, args[0], "default")) {
+            break :query try data.loadDefault() orelse return fatal("no default Zig version", .{});
+        } else {
+            break :query ZigInstallation.Query.parse(args[0]) catch return fatal("invalid Zig version", .{});
+        }
+    };
+    const zig_index = findMatchingInstallationIndex(available, query) orelse
         return fatal("no Zig matching '{s}'", .{args[0]});
     try execZig(gpa, available[zig_index], args[1..]);
 }
 
 fn exeZig(gpa: Allocator, args: []const []const u8) !noreturn {
-    const available = loadAvailableInstallations(gpa) catch |err|
-        return fatal("failed to load Zig installations: {}", .{err});
+    var data = try AppData.open(gpa);
+    defer data.close();
+    const available = try data.loadAvailableInstallations(gpa);
     defer gpa.free(available);
-    const zig_index = findMatchingInstallationIndex(available, .default) orelse return fatal("no Zig version installed", .{});
+    const default = try data.loadDefault() orelse return fatal("no default Zig version", .{});
+    const zig_index = findMatchingInstallationIndex(available, default) orelse
+        return fatal("no Zig version matching default '{}'", .{default});
     try execZig(gpa, available[zig_index], args);
 }
 
@@ -226,13 +279,14 @@ fn graftZig(gpa: Allocator, path: []const u8) !void {
     const raw_hash = try runProcess(gpa, &.{ exe_path, "fetch", path });
     defer gpa.free(raw_hash);
     const hash = std.mem.trimRight(u8, raw_hash, "\r\n");
-
-    const new_zig = ZigInstallation.init(version, hash, false) catch
+    const new_zig = ZigInstallation.init(version, hash) catch
         return fatal("invalid Zig version '{s}', hash '{s}'", .{ version, hash });
-    const available = loadAvailableInstallations(gpa) catch |err|
-        return fatal("failed to load Zig installations: {}", .{err});
+
+    var data = try AppData.open(gpa);
+    defer data.close();
+    const available = try data.loadAvailableInstallations(gpa);
     defer gpa.free(available);
-    if (findMatchingInstallationIndex(available, .{ .exact = version })) |existing_index| {
+    if (findMatchingInstallationIndex(available, .{ .exact = new_zig.version })) |existing_index| {
         const existing_zig = available[existing_index];
         if (!std.mem.eql(u8, existing_zig.hash.slice(), hash)) {
             return fatal("Zig '{s}' is already installed with conflicting hash", .{version});
@@ -243,8 +297,7 @@ fn graftZig(gpa: Allocator, path: []const u8) !void {
     defer gpa.free(new_available);
     @memcpy(new_available[0..available.len], available);
     new_available[available.len] = new_zig;
-    saveAvailableInstallations(gpa, new_available) catch |err|
-        return fatal("failed to save Zig installations: {}", .{err});
+    try data.saveAvailableInstallations(new_available);
 }
 
 fn runProcess(gpa: Allocator, argv: []const []const u8) ![]u8 {
@@ -288,37 +341,125 @@ fn formatArgv(
     }
 }
 
+const AppData = struct {
+    dir: std.fs.Dir,
+
+    fn open(gpa: Allocator) !AppData {
+        var parent_dir = try known_folders.open(gpa, .data, .{}) orelse return error.FileNotFound;
+        defer parent_dir.close();
+        return .{ .dir = try parent_dir.makeOpenPath("zigman", .{}) };
+    }
+
+    fn close(data: *AppData) void {
+        data.dir.close();
+        data.* = undefined;
+    }
+
+    fn loadDefault(data: *AppData) !?ZigInstallation.Query {
+        var file = data.dir.openFile("default", .{}) catch |err| switch (err) {
+            error.FileNotFound => return null,
+            else => |other| return other,
+        };
+        defer file.close();
+        var buf: [ZigInstallation.max_version_len + 2]u8 = undefined;
+        var len = try file.readAll(&buf);
+        if (len > 0 and buf[len - 1] == '\n') len -= 1;
+        return try .parse(buf[0..len]);
+    }
+
+    fn saveDefault(data: *AppData, default: ZigInstallation.Query) !void {
+        var file = try data.dir.createFile("default", .{});
+        defer file.close();
+        try file.writer().print("{}\n", .{default});
+        try file.sync();
+    }
+
+    fn loadAvailableInstallations(data: *AppData, gpa: Allocator) ![]ZigInstallation {
+        var file = data.dir.openFile("available", .{}) catch |err| switch (err) {
+            error.FileNotFound => return &.{},
+            else => |other| return other,
+        };
+        defer file.close();
+        var br = std.io.bufferedReader(file.reader());
+        const reader = br.reader();
+
+        var available: std.ArrayList(ZigInstallation) = .init(gpa);
+        defer available.deinit();
+        var buf: [256]u8 = undefined; // enough to hold any valid line
+        while (reader.readUntilDelimiter(&buf, '\n')) |line| {
+            try available.append(try .parse(line));
+        } else |err| switch (err) {
+            error.EndOfStream => {},
+            error.StreamTooLong => return error.InvalidInstallation,
+            else => |other| return other,
+        }
+        return try available.toOwnedSlice();
+    }
+
+    fn saveAvailableInstallations(data: *AppData, available: []const ZigInstallation) !void {
+        var file = try data.dir.createFile("available", .{});
+        defer file.close();
+        var bw = std.io.bufferedWriter(file.writer());
+        const writer = bw.writer();
+        for (available) |zig| {
+            try writer.print("{}\n", .{zig});
+        }
+        try bw.flush();
+        try file.sync();
+    }
+};
+
 const ZigInstallation = struct {
     version: Version,
     hash: Hash,
-    default: bool,
 
-    const Version = std.BoundedArray(u8, 32);
+    const max_version_len = 32;
+    const Version = std.BoundedArray(u8, max_version_len);
     // Current Zig package hashes are always 68 bytes. However, if
     // https://github.com/ziglang/zig/issues/20178 is implemented, it could be
     // variable up to 75 bytes. Increasing this to 80 makes it a nice round
     // number.
-    const Hash = std.BoundedArray(u8, 80);
+    const max_hash_len = 80;
+    const Hash = std.BoundedArray(u8, max_hash_len);
 
     const Query = union(enum) {
-        default,
         master,
         latest,
         mach_latest,
-        exact: []const u8,
+        exact: Version,
 
-        fn parse(s: []const u8) Query {
-            if (std.mem.eql(u8, s, "default")) {
-                return .default;
-            } else if (std.mem.eql(u8, s, "master")) {
+        fn parse(s: []const u8) error{InvalidInstallation}!Query {
+            if (std.mem.eql(u8, s, "master")) {
                 return .master;
             } else if (std.mem.eql(u8, s, "latest")) {
                 return .latest;
             } else if (std.mem.eql(u8, s, "mach-latest")) {
                 return .mach_latest;
             } else {
-                return .{ .exact = s };
+                const version = Version.fromSlice(s) catch return error.InvalidInstallation;
+                _ = std.SemanticVersion.parse(version.slice()) catch return error.InvalidInstallation;
+                return .{ .exact = version };
             }
+        }
+
+        fn unparse(query: Query) []const u8 {
+            return switch (query) {
+                .master => "master",
+                .latest => "latest",
+                .mach_latest => "mach-latest",
+                .exact => |version| version.slice(),
+            };
+        }
+
+        pub fn format(
+            query: Query,
+            comptime fmt: []const u8,
+            options: std.fmt.FormatOptions,
+            writer: anytype,
+        ) @TypeOf(writer).Error!void {
+            _ = fmt;
+            _ = options;
+            try writer.writeAll(query.unparse());
         }
     };
 
@@ -329,12 +470,11 @@ const ZigInstallation = struct {
         return std.SemanticVersion.parse(zig.version.slice()) catch unreachable;
     }
 
-    fn init(version: []const u8, hash: []const u8, default: bool) error{InvalidInstallation}!ZigInstallation {
+    fn init(version: []const u8, hash: []const u8) error{InvalidInstallation}!ZigInstallation {
         _ = std.SemanticVersion.parse(version) catch return error.InvalidInstallation;
         return .{
             .version = Version.fromSlice(version) catch return error.InvalidInstallation,
             .hash = Hash.fromSlice(hash) catch return error.InvalidInstallation,
-            .default = default,
         };
     }
 
@@ -342,79 +482,31 @@ const ZigInstallation = struct {
         var parts = std.mem.splitScalar(u8, s, ' ');
         const version = parts.first();
         const hash = parts.next() orelse return error.InvalidInstallation;
-        var default = false;
-        while (parts.next()) |part| {
-            if (default or !std.mem.eql(u8, part, "default")) return error.InvalidInstallation;
-            default = true;
-        }
-        return init(version, hash, default);
+        if (parts.next() != null) return error.InvalidInstallation;
+        return init(version, hash);
     }
 
     test parse {
         try std.testing.expectEqualDeep(ZigInstallation{
             .version = Version.fromSlice("0.13.0") catch unreachable,
             .hash = Hash.fromSlice("122095c9b2703250317da71eb14a2979a398ec776b42a979d5ffbf0cc5100a77e36b") catch unreachable,
-            .default = true,
-        }, try ZigInstallation.parse("0.13.0 122095c9b2703250317da71eb14a2979a398ec776b42a979d5ffbf0cc5100a77e36b default"));
+        }, try ZigInstallation.parse("0.13.0 122095c9b2703250317da71eb14a2979a398ec776b42a979d5ffbf0cc5100a77e36b"));
     }
 
-    fn write(zig: ZigInstallation, writer: anytype) !void {
+    pub fn format(
+        zig: ZigInstallation,
+        comptime fmt: []const u8,
+        options: std.fmt.FormatOptions,
+        writer: anytype,
+    ) @TypeOf(writer).Error!void {
+        _ = fmt;
+        _ = options;
         try writer.print("{s} {s}", .{ zig.version.slice(), zig.hash.slice() });
-        if (zig.default) {
-            try writer.writeAll(" default");
-        }
     }
 };
 
-fn loadAvailableInstallations(gpa: Allocator) ![]ZigInstallation {
-    var file = file: {
-        var data_dir = try makeOpenAppDir(gpa, .data);
-        defer data_dir.close();
-        break :file data_dir.openFile("available", .{}) catch |err| switch (err) {
-            error.FileNotFound => return &.{},
-            else => |other| return other,
-        };
-    };
-    defer file.close();
-    var br = std.io.bufferedReader(file.reader());
-    const reader = br.reader();
-
-    var available: std.ArrayList(ZigInstallation) = .init(gpa);
-    defer available.deinit();
-    var buf: [256]u8 = undefined; // enough to hold any valid line
-    while (reader.readUntilDelimiter(&buf, '\n')) |line| {
-        try available.append(try .parse(line));
-    } else |err| switch (err) {
-        error.EndOfStream => {},
-        error.StreamTooLong => return error.InvalidInstallation,
-        else => |other| return other,
-    }
-    return try available.toOwnedSlice();
-}
-
-fn saveAvailableInstallations(gpa: Allocator, available: []const ZigInstallation) !void {
-    var file = file: {
-        var data_dir = try makeOpenAppDir(gpa, .data);
-        defer data_dir.close();
-        break :file try data_dir.createFile("available", .{});
-    };
-    defer file.close();
-    var bw = std.io.bufferedWriter(file.writer());
-    const writer = bw.writer();
-    for (available) |zig| {
-        try zig.write(writer);
-        try writer.writeByte('\n');
-    }
-    try bw.flush();
-}
-
 fn findMatchingInstallationIndex(available: []const ZigInstallation, query: ZigInstallation.Query) ?usize {
     switch (query) {
-        .default => {
-            return for (available, 0..) |zig, i| {
-                if (zig.default) break i;
-            } else null;
-        },
         .master => {
             return findNewestInstallationIndexMatching(available, struct {
                 fn isApplicable(version: std.SemanticVersion) bool {
@@ -438,7 +530,7 @@ fn findMatchingInstallationIndex(available: []const ZigInstallation, query: ZigI
         },
         .exact => |version| {
             return for (available, 0..) |zig, i| {
-                if (std.mem.eql(u8, version, zig.version.slice())) break i;
+                if (std.mem.eql(u8, version.slice(), zig.version.slice())) break i;
             } else null;
         },
     }
